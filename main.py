30e2c237cde28dd18518432d2307adc62c0a10c6
ZatokaV <zatokav@gmail.com>
2023-03-29 16:52:42 +0300

    add healthchecker

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException, status
+from sqlalchemy import text
+from sqlalchemy.orm import Session
+
+from src.database.connect import get_db
+from src.services.messages_templates import DB_CONFIG_ERROR, DB_CONNECT_ERROR, WELCOME_MESSAGE
 
 app = FastAPI()
+
+
+@app.get("/api/healthchecker")
+def healthchecker(db: Session = Depends(get_db)):
+    try:
+        result = db.execute(text("SELECT 1")).fetchone()
+        print(result)
+        if result is None:
+            raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                                detail=DB_CONFIG_ERROR)
+        return {"message": WELCOME_MESSAGE}
+    except Exception as e:
+        print(e)
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail=DB_CONNECT_ERROR)
 
 
 @app.get("/", name='Home')
