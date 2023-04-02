3654673f4c7d5f5ac19d43c71bb0034db7d61272
ZatokaV <zatokav@gmail.com>
2023-04-02 16:36:18 +0300

    Update users.py

--- src/routes/users.py
+++ src/routes/users.py
@@ -28,3 +28,6 @@ async def get_user_profile(username: str, db: Session = Depends(get_db)):
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND, detail=NOT_FOUND)
     return user_profile
+
+
+
