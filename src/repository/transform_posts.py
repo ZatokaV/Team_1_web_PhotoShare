14f2c3d9ec424bc8cbd57787db37bd001f004bba
IrynaShushkevych <IraOlijnyk@gmail.com>
2023-04-02 11:31:45 +0300

    docstring

--- src/repository/transform_posts.py
+++ src/repository/transform_posts.py
@@ -6,6 +6,17 @@ from src.database.models import TransformPosts, Post, User, UserRole
 
 
 async def get_image_for_transform(image_id: int, current_user: User, db: Session) -> str | None:
+    """
+    The get_image_for_transform function is used to retrieve the image path for a given image id.
+        The function takes in an integer representing the id of the desired image, a User object representing
+        the current user, and a Session object that represents our database connection.  If we are able to find
+        an image with this ID in our database (and if we are not admin), then we return its photo_url attribute.
+
+    :param image_id: int: Get the image from the database
+    :param current_user: User: Check if the user is an admin or not
+    :param db: Session: Access the database
+    :return: The path to the image that will be transformed
+    """
     if current_user.user_role == UserRole.Admin.name:
         image = db.query(Post).filter(Post.id == image_id).first()
     else:
@@ -17,6 +28,17 @@ async def get_image_for_transform(image_id: int, current_user: User, db: Session
 
 
 async def set_transform_image(image_id: int, modify_url: str, current_user: User, db: Session) -> TransformPosts | None:
+    """
+    The set_transform_image function takes in an image_id, modify_url, current_user and db.
+        If the user is an admin or if the user owns the photo then it will create a new TransformPosts object with
+        photo url and id. It will add this to the database and commit it.
+
+    :param image_id: int: Identify the image that will be modified
+    :param modify_url: str: Store the url of the modified image
+    :param current_user: User: Check if the user is an admin or not
+    :param db: Session: Access the database
+    :return: A transform posts object or none
+    """
     image = None
     if current_user.user_role == UserRole.Admin.name:
         photo = db.query(Post).filter(Post.id == image_id).first()
@@ -31,6 +53,17 @@ async def set_transform_image(image_id: int, modify_url: str, current_user: User
 
 
 async def get_transform_image(image_id: int, current_user: User, db: Session) -> TransformPosts | None:
+    """
+    The get_transform_image function is used to retrieve a single image from the database.
+        The function takes in an image_id, current_user and db as parameters.
+        If the user is an admin, then it will return all images with that id.
+        Otherwise, it will only return images with that id if they were created by the current user.
+
+    :param image_id: int: Get the image id from the database
+    :param current_user: User: Check if the user is an admin or not
+    :param db: Session: Access the database
+    :return: An image from the database
+    """
     if current_user.user_role == UserRole.Admin.name:
         img = db.query(TransformPosts).filter(TransformPosts.id == image_id).first()
     else:
@@ -40,6 +73,17 @@ async def get_transform_image(image_id: int, current_user: User, db: Session) ->
 
 
 async def remove_transform_image(image_id: int, current_user: User, db: Session) -> TransformPosts | None:
+    """
+    The remove_transform_image function is used to remove a transform image from the database.
+        The function takes in an image_id and current_user as parameters, and returns the removed TransformPosts object.
+        If no such TransformPosts object exists, None is returned instead.
+
+    :param image_id: int: Specify the image id of the image that is to be removed
+    :param current_user: User: Check if the user is an admin or not
+    :param db: Session: Access the database
+    :return: The image that was removed, or none if it failed
+    :doc-author: Trelent
+    """
     if current_user.user_role == UserRole.Admin.name:
         img = db.query(TransformPosts).filter(TransformPosts.id == image_id).first()
     else:
@@ -52,6 +96,17 @@ async def remove_transform_image(image_id: int, current_user: User, db: Session)
 
 
 async def get_all_transform_images(image_id: int, current_user: User, db: Session) -> List[TransformPosts]:
+    """
+    The get_all_transform_images function returns a list of all transform images for the given image id.
+        If the current user is an admin, then it will return all transform images for that image id.
+        Otherwise, it will only return those transform images that belong to the current user.
+
+    :param image_id: int: Filter the transform posts table by photo_id
+    :param current_user: User: Check if the user is an admin or not
+    :param db: Session: Access the database
+    :return: A list of all the images that are transformed from an image
+    :doc-author: Trelent
+    """
     if current_user.user_role == UserRole.Admin.name:
         list_image = db.query(TransformPosts).filter(TransformPosts.photo_id == image_id).all()
     else:
--- src/routes/transform_posts.py
+++ src/routes/transform_posts.py
@@ -24,6 +24,15 @@ router = APIRouter(prefix='/image/transform', tags=['Transform Image'])
 @router.get('/user', response_model=List[TransformImageResponse], status_code=status.HTTP_200_OK)
 async def get_list_of_transformed_for_user(current_user: User = Depends(auth_service.get_current_user),
                                            db: Session = Depends(get_db)):
+    """
+    The get_list_of_transformed_for_user function returns a list of all the transformed images for a given user.
+        The function takes in an optional current_user parameter, which is the currently logged-in user.
+        If no current_user is provided, then it will default to None.
+
+    :param current_user: User: Get the user who is currently logged in
+    :param db: Session: Get the database session
+    :return: A list of all transformed images for the current user
+    """
     return await rep_transform.get_all_transform_images_for_user(current_user, db)
 
 
@@ -33,7 +42,20 @@ async def transformation_for_image(base_image_id: int, body: TransformImageModel
                                    current_user: User = Depends(
                                        auth_service.get_current_user),
                                    db: Session = Depends(get_db)):
-    # image_url = 'PythonContactsApp/Irina'
+    """
+    The transformation_for_image function takes in a base_image_id, body, current_user and db.
+    The function then calls the get_image_for transform method from the repos/transformations.py file to retrieve an image url for transformation.
+    If no image is found with that id, it raises a 404 error message saying &quot;Image not found&quot;.
+    Otherwise it configures cloudinary using my api key and secret key (which I have hidden).
+    It then creates a list of transformations based on what was passed into the body of this request (the user's desired transformations).
+    Finally it builds an url for that
+
+    :param base_image_id: int: Get the image from the database
+    :param body: TransformImageModel: Get the transformation parameters from the request body
+    :param current_user: User: Get the current user from the database
+    :param db: Session: Get a database session
+    :return: A url of the transformed image
+    """
     image_url = await rep_transform.get_image_for_transform(base_image_id, current_user, db)
     if image_url is None:
         raise HTTPException(
@@ -55,6 +77,18 @@ async def save_transform_image(base_image_id: int, body: SaveTransformImageModel
                                current_user: User = Depends(
                                    auth_service.get_current_user),
                                db: Session = Depends(get_db)):
+    """
+    The save_transform_image function is used to save a transformed image.
+        The function takes in the base_image_id, body, current user and database as parameters.
+        It then calls the set_transform_image function from rep transform which saves the transformed image into
+        the database and returns it if successful or None otherwise.
+
+    :param base_image_id: int: Specify the id of the image that is being transformed
+    :param body: SaveTransformImageModel: Pass the url of the image to be saved
+    :param current_user: User: Get the current user
+    :param db: Session: Get the database session
+    :return: The image with the specified id
+    """
     img = await rep_transform.set_transform_image(base_image_id, body.url, current_user, db)
     if img is None:
         raise HTTPException(
@@ -65,7 +99,16 @@ async def save_transform_image(base_image_id: int, body: SaveTransformImageModel
 @router.get('/qrcode/{transform_image_id}', status_code=status.HTTP_200_OK)
 async def get_qrcode_for_transform_image(transform_image_id: int, current_user: User = Depends(auth_service.get_current_user),
                                          db: Session = Depends(get_db)):
-    # image_url = 'https://res.cloudinary.com/drilpksk7/image/upload/e_grayscale:100/v1/PythonContactsApp/Irina'
+    """
+    The get_qrcode_for_transform_image function is used to generate a QR code for the transform image.
+    The function takes in an integer representing the id of the transform image and returns a string containing
+    the base64 encoded QR code.
+
+    :param transform_image_id: int: Get the image_url from the database
+    :param current_user: User: Get the current user
+    :param db: Session: Access the database
+    :return: A base64 encoded qr code
+    """
     image_url = await rep_transform.get_transform_image(transform_image_id, current_user, db)
     if image_url is None:
         raise HTTPException(
@@ -80,6 +123,17 @@ async def get_qrcode_for_transform_image(transform_image_id: int, current_user: 
 @router.get('/{transform_image_id}', response_model=TransformImageResponse, status_code=status.HTTP_200_OK)
 async def get_transformed_image(transform_image_id: int, current_user: User = Depends(auth_service.get_current_user),
                                 db: Session = Depends(get_db)):
+    """
+    The get_transformed_image function returns a transformed image.
+        The function takes in the transform_image_id and current user as parameters.
+        It then calls the get_transform_image function from repos/transformations to retrieve the transformed image.
+        If no such image is found, it raises an HTTPException with status code 404 and detail NOT FOUND.
+
+    :param transform_image_id: int: Get the image id from the url
+    :param current_user: User: Get the current user from the auth_service
+    :param db: Session: Pass the database session to the function
+    :return: A transformed image
+    """
     img = await rep_transform.get_transform_image(transform_image_id, current_user, db)
     if img is None:
         raise HTTPException(
@@ -90,6 +144,16 @@ async def get_transformed_image(transform_image_id: int, current_user: User = De
 @router.delete('/{transform_image_id}', status_code=status.HTTP_204_NO_CONTENT)
 async def remove_transformed_image(transform_image_id: int, current_user: User = Depends(auth_service.get_current_user),
                                    db: Session = Depends(get_db)):
+    """
+    The remove_transformed_image function is used to remove a transformed image from the database.
+        The function takes in an integer representing the id of the transform_image object that will be removed,
+        and returns a JSON response containing information about whether or not it was successful.
+
+    :param transform_image_id: int: Identify the image that is to be removed
+    :param current_user: User: Get the user that is currently logged in
+    :param db: Session: Pass the database session to the repository
+    :return: An image object
+    """
     img = await rep_transform.remove_transform_image(transform_image_id, current_user, db)
     if img is None:
         raise HTTPException(
@@ -100,6 +164,16 @@ async def remove_transformed_image(transform_image_id: int, current_user: User =
 async def get_list_of_transformed_for_image(base_image_id: int,
                                             current_user: User = Depends(auth_service.get_current_user),
                                             db: Session = Depends(get_db)):
+    """
+    The get_list_of_transformed_for_image function returns a list of all transformed images for the given base image.
+        The function takes in an integer representing the id of the base image and returns a list of dictionaries, each
+        dictionary containing information about one transformed image.
+
+    :param base_image_id: int: Get the base image id from the url
+    :param current_user: User: Get the user that is currently logged in
+    :param db: Session: Access the database
+    :return: A list of transformed images for the base image id
+    """
     return await rep_transform.get_all_transform_images(base_image_id, current_user, db)
 
 
