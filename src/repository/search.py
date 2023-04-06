f08a3bf4b607540ffb799318fa9f12493fb382ca
IrynaShushkevych <IraOlijnyk@gmail.com>
2023-04-06 11:37:53 +0300

    create search users

--- src/repository/search.py
+++ src/repository/search.py
@@ -1,11 +1,15 @@
+from typing import List
+
 from sqlalchemy import or_, func, text, desc
 from sqlalchemy.orm import Session
 
 from src.database.models import Post, Tag, post_tag, RatePost, User
 from src.services.cloudynary import get_url
+from src.schemas import SearchResponse, SortUserType, SortType
 
 
-async def get_search_posts(search_str: str, sort: str, sort_type: int, skip: int, limit: int, db: Session):
+async def get_search_posts(search_str: str, sort: str, sort_type: int, skip: int, limit: int, db: Session)\
+        -> List[SearchResponse]:
     search_list = []
     search_list.append(Post.description.ilike(f'%{search_str}%'))
     search_list.append(Tag.tag.ilike(f'%{search_str}%'))
@@ -13,18 +17,17 @@ async def get_search_posts(search_str: str, sort: str, sort_type: int, skip: int
         .select_from(Post).join(User).join(RatePost, isouter=True).join(post_tag, isouter=True).join(Tag, isouter=True)\
         .filter(or_(*search_list)) \
         .group_by(Post, User.username)
-    if sort == 'rate':
+    if sort == SortType.rate.name:
         if sort_type == -1:
             sql = sql.order_by(desc('rate'))
         else:
             sql = sql.order_by('rate')
-    if sort == 'date':
+    if sort == SortType.date.name:
         if sort_type == -1:
             sql = sql.order_by(desc(Post.created_at))
         else:
             sql = sql.order_by(Post.created_at)
-    print(sql)
-    posts = sql.all()
+    posts = sql.offset(skip).limit(limit).all()
     result = []
     for post in posts:
         item = {x.name: getattr(post[0], x.name) for x in post[0].__table__.columns}
@@ -32,7 +35,37 @@ async def get_search_posts(search_str: str, sort: str, sort_type: int, skip: int
         item['rate'] = post[2]
         item['photo_url'] = get_url(item['photo_url'])
         tags = db.query(Tag).join(post_tag).join(Post).filter(Post.id == item['id']).order_by(Tag.tag).all()
-        # item['tags'] = [{x.name: getattr(tag, x.name) for x in tag.__table__.columns} for tag in tags]
         item['tags'] = [{'id': tag.id, 'tag': tag.tag} for tag in tags]
         result.append(item)
     return result
+
+
+async def get_search_users(search_str: str, sort: str, sort_type: int, skip: int, limit: int, db: Session):
+    list_reg = []
+    list_reg.append(User.username.ilike(f"%{search_str}%"))
+    list_reg.append(User.first_name.ilike(f"%{search_str}%"))
+    list_reg.append(User.last_name.ilike(f"%{search_str}%"))
+    list_reg.append(User.email.ilike(f"%{search_str}%"))
+    sql = db.query(User).filter(or_(*list_reg))
+    if sort == SortUserType.username.name:
+        if sort_type == -1:
+            sql = sql.order_by(desc(SortUserType.username.name))
+        else:
+            sql = sql.order_by(SortUserType.username.name)
+    if sort == SortUserType.date.name:
+        if sort_type == -1:
+            sql = sql.order_by(desc(User.created_at))
+        else:
+            sql = sql.order_by(User.created_at)
+    if sort == SortUserType.email.name:
+        if sort_type == -1:
+            sql = sql.order_by(desc(User.email))
+        else:
+            sql = sql.order_by(User.email)
+    if sort == SortUserType.name.name:
+        if sort_type == -1:
+            sql = sql.order_by(desc(User.first_name, User.last_name))
+        else:
+            sql = sql.order_by(User.first_name, User.last_name)
+    users = sql.offset(skip).limit(limit).all()
+    return users
